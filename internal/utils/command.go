bfba05abb3393346bce6df73a783925f9499a213
vlatan <33163699+vlatan@users.noreply.github.com>
2026-01-10 17:18:23 +0100

    Allow stdout when running commands

--- internal/utils/command.go
+++ internal/utils/command.go
@@ -11,14 +11,11 @@ func RunCommand(name string, args ...string) error {
 	// Init the command
 	cmd := exec.Command(name, args...)
 
-	// Point Stdin and Stderr streams to terminal
+	// Point the command Stdin, Stdout and Stderr streams
+	// to the user OS corresponding streams
 	cmd.Stdin = os.Stdin   // Allow interactive input
+	cmd.Stdout = os.Stdout // Show the output
 	cmd.Stderr = os.Stderr // Show errors directly to the terminal
 
-	// Run the command
-	if err := cmd.Run(); err != nil {
-		return err
-	}
-
-	return nil
+	return cmd.Run()
 }
