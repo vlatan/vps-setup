dbb5cb178e146d208d081a5afc8819b0ee0611dc
vlatan <33163699+vlatan@users.noreply.github.com>
2026-01-11 21:39:53 +0100

    Finish format bash

--- internal/settings/bash.go
+++ internal/settings/bash.go
@@ -68,6 +68,90 @@ func (b *Bash) CreateAliases() error {
 	return nil
 }
 
+func (b *Bash) CreatePrompt() error {
+
+	// Backslashes and quotes escaped
+	promptContent := []string{
+		"# Using tput for colors and formatting. Reset colors.",
+		"tput sgr0",
+		"",
+		"# Set color variables",
+		"bold=\"\\[$(tput bold)\\]\"",
+		"reset=\"\\[$(tput sgr0)\\]\"",
+		"blue=\"\\[$(tput setaf 153)\\]\"",
+		"steel_blue=\"\\[$(tput setaf 67)\\]\"",
+		"green=\"\\[$(tput setaf 71)\\]\"",
+		"orange=\"\\[$(tput setaf 166)\\]\"",
+		"red=\"\\[$(tput setaf 167)\\]\"",
+		"white=\"\\[$(tput setaf 15)\\]\"",
+		"yellow=\"\\[$(tput setaf 228)\\]\"",
+		"",
+		"# Highlight the user name when logged in as root.",
+		"[[ \"${USER}\" == \"root\" ]] && userColor=\"${red}\" || userColor=\"${green}\"",
+		"",
+		"# Highlight the hostname when connected via SSH.",
+		"[[ \"${SSH_TTY}\" ]] && hostColor=\"${red}\" || hostColor=\"${orange}\"",
+		"",
+		"# Set the default interaction prompt",
+		"PS1=\"${bold}\"							                    # bold",
+		"PS1+=\"${white}[${yellow}\\A${white}]\"				        # [time]",
+		"PS1+=\"[${userColor}\\u${white}@${hostColor}\\h${white}]\"		# [user@host]",
+		"PS1+=\"[${steel_blue}\\w${white}]\"				            # [pwd]",
+		"PS1+=\"\\n$ ${reset}\"                                       	# new line and reset",
+		"export PS1",
+		"",
+		"# Set the continuation interactive prompt",
+		"PS2=\"${yellow} → ${reset}\"",
+		"export PS2",
+	}
+
+	data := []byte(strings.Join(promptContent, "\n") + "\n")
+	if err := utils.WriteFile(b.root, b.prompt, data); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// FormatBashrc appends content to .bashrc file.
+// Enables aliases and the custom prompt.
+func (b *Bash) FormatBashrc() error {
+
+	bashrcContent := []string{
+		"# use aliases from file if any",
+		"if [ -f ~/.bash_aliases ]; then",
+		"    . ~/.bash_aliases",
+		"fi",
+		"",
+		"# use custom prompt from file if any",
+		"if [ -f ~/.bash_prompt ]; then",
+		"    . ~/.bash_prompt",
+		"fi",
+		"",
+		"# increase bash history",
+		"export HISTSIZE=10000000",
+		"export HISTFILESIZE=10000000",
+		"",
+		"# set more restrictive user mask",
+		"umask 022",
+	}
+
+	// Append the content to .bashrc
+	flag := os.O_APPEND | os.O_CREATE | os.O_WRONLY
+	file, err := b.root.OpenFile(b.bashrc, flag, 0644)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	data := "\n" + strings.Join(bashrcContent, "\n") + "\n"
+	if _, err := file.WriteString(data); err != nil {
+		return err
+	}
+
+	return nil
+}
+
 // FormatBash configures the bash experience
 // by creating custom aliases and prompt.
 func FormatBash(username string) error {
@@ -81,9 +165,16 @@ func FormatBash(username string) error {
 	defer home.Close()
 
 	bash := NewBash(home)
+	callables := []func() error{
+		bash.CreateAliases,
+		bash.CreatePrompt,
+		bash.FormatBashrc,
+	}
 
-	if err = bash.CreateAliases(); err != nil {
-		return err
+	for _, Callable := range callables {
+		if err := Callable(); err != nil {
+			return err
+		}
 	}
 
 	return nil
