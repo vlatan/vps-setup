96c89620db61551b76edbef35d37e4c42b35ede5
vlatan <33163699+vlatan@users.noreply.github.com>
2026-01-14 15:58:11 +0100

    USe env vars for postfix

--- internal/setup/postfix.go
+++ internal/setup/postfix.go
@@ -20,56 +20,56 @@ func (s *Setup) SetupPostfix() error {
 		return nil
 	}
 
-	var domain string
 	for {
-		domain = utils.AskQuestion("MAILNAME (domain): ", s.Scanner)
-		if domain != "" {
+		if s.PostfixMailname != "" {
 			break
 		}
+		s.PostfixMailname = utils.AskQuestion(
+			"MAILNAME (domain): ",
+			s.Scanner,
+		)
 	}
 
-	var smtpHost string
 	for {
-		smtpHost = utils.AskQuestion("SMTP_HOST: ", s.Scanner)
-		if smtpHost != "" {
+		if s.SMTPHost != "" {
 			break
 		}
+		s.SMTPHost = utils.AskQuestion("SMTP_HOST: ", s.Scanner)
 	}
 
-	var smtpPort string
 	for {
-		smtpPort = utils.AskQuestion("SMTP_PORT: ", s.Scanner)
-		if smtpPort != "" {
+		if s.SMTPPort != "" {
 			break
 		}
+		s.SMTPPort = utils.AskQuestion("SMTP_PORT: ", s.Scanner)
 	}
 
-	var smtpUsername string
 	for {
-		smtpUsername, err := utils.AskPassword("SMTP_USERNAME: ")
+		if s.SMTPUsername != "" {
+			break
+		}
+		var err error
+		s.SMTPUsername, err = utils.AskPassword("SMTP_USERNAME: ")
 		if err != nil {
 			return err
 		}
-
-		if smtpUsername != "" {
-			break
-		}
 	}
 
-	var smtpPassword string
 	for {
-		smtpPassword, err := utils.AskPassword("SMTP_PASSWORD: ")
+		if s.SMTPPassword != "" {
+			break
+		}
+		var err error
+		s.SMTPPassword, err = utils.AskPassword("SMTP_PASSWORD: ")
 		if err != nil {
 			return err
 		}
-
-		if smtpPassword != "" {
-			break
-		}
 	}
+
+	fmt.Println("Setting up Postfix SMTP relay...")
 
 	stdins := []string{
-		fmt.Sprintf("postfix postfix/mailname string %s\n", domain),
+		fmt.Sprintf("postfix postfix/mailname string %s\n", s.PostfixMailname),
 		"postfix postfix/main_mailer_type string 'Internet Site'\n",
 	}
 
@@ -97,7 +97,10 @@ func (s *Setup) SetupPostfix() error {
 
 	// Write to the file
 	name := "postfix/sasl/sasl_passwd"
-	data := fmt.Sprintf("[%s]:%s %s:%s", smtpHost, smtpPort, smtpUsername, smtpPassword)
+	data := fmt.Sprintf(
+		"[%s]:%s %s:%s",
+		s.SMTPHost, s.SMTPPort, s.SMTPUsername, s.SMTPPassword,
+	)
 	if err := utils.WriteFile(s.Etc, name, []byte(data)); err != nil {
 		return err
 	}
@@ -114,8 +117,8 @@ func (s *Setup) SetupPostfix() error {
 	}
 
 	postConfs := []string{
-		fmt.Sprintf("myhostname = %s", domain),
-		fmt.Sprintf("relayhost = [%s]:%s", smtpHost, smtpPort),
+		fmt.Sprintf("myhostname = %s", s.PostfixMailname),
+		fmt.Sprintf("relayhost = [%s]:%s", s.SMTPHost, s.SMTPPort),
 		"smtp_sasl_auth_enable = yes",
 		"smtp_sasl_security_options = noanonymous",
 		fmt.Sprintf("smtp_sasl_password_maps = hash:%s", saslPaswdFile),
